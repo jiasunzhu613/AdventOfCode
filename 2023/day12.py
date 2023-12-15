0081341718e341ead792f44dd42a91bb7693bfb5
jonathanzhu <jiasunzhu@gmail.com>
2023-12-15 14:35:48 -0500

    Day 14!

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,6 +1,7 @@
-import re
+import re, itertools, sys
+sys.setrecursionlimit(10**5)
 
-file = open("Inputs/day12.txt", "r")
+file = open("../input.txt", "r")
 input = [i.strip().split() for i in file.readlines()]
 
 springs = []
@@ -9,57 +10,81 @@ for spring, damaged in input:
     springs.append(spring)
     damaged_segments.append(list(map(int, damaged.split(","))))
 
-print(springs)
-print(damaged_segments)
-
-
+# IDEAS:
+# for each index of broken springs
+# split the needed orientation with ?'s by . then solve each component and multiply eachother together
 def equivalent(word1, word2):
+    if len(word1) != len(word2):
+        return False
     for i in range(len(word1)):
         if word1[i] != word2[i] and word1[i] != "?":
             return False
-    print(word2)
+    # print(word2)
     return True
 
-def solvePart1():
-    ret = 0
-    for i in range(len(springs)):
-        added = set()
-        least_length = sum(damaged_segments[i]) + len(damaged_segments[i]) - 1
-        segments = ["#" * length for length in damaged_segments[i]]
-        optimal = ".".join(segments)
+memo = {}
+def helper(to_add, ind, segment, optimal):  # solves number of orientations for a small segment
+    global memo
+    # print((to_add, segment, optimal))
+    if len(optimal) > len(segment):
+        return 0
+    if (to_add, ind, optimal) in memo:
+        return memo[(to_add, ind, optimal)]
+    if ind == len(segment):
+        return 0
+    if to_add == 0:
+        return equivalent(segment, optimal)
 
-        print(optimal)
-        extra_length = len(springs[i]) - least_length
-        queue = [(extra_length, optimal)]
-        while queue:
-            to_add, string = queue.pop(-1)
-            if to_add == 0:
-                ret += equivalent(springs[i], string)
-                continue
+    new_add = optimal[:ind] + "." + optimal[ind:]
+    add = helper(to_add - 1, ind + 1, segment, new_add)
+    dont = helper(to_add, ind + 1, segment, optimal)
+    total = add + dont
 
-            if "." + string not in added:
-                queue.append((to_add - 1, "." + string))
-                added.add("." + string)
-                # print(queue[-1])
-            if string + "." not in added:
-                queue.append((to_add - 1, string + "."))
-                added.add(string + ".")
-                # print(queue[-1])
+    if (to_add, ind, optimal) not in memo:
+        memo[(to_add, ind, optimal)] = total
+    return memo[(to_add, ind, optimal)]
 
-            broken = False
-            for j in range(len(string)):
-                if string[j] == "." and broken:
-                    if string[:j] + "." + string[j:] not in added:
-                        queue.append((to_add - 1, string[:j] + "." + string[j:]))
-                        added.add(string[:j] + "." + string[j:])
-                    # print(queue[-1])
-                    broken = not broken
-                elif string[j] == "#" and not broken:
-                    broken = not broken
+def naive(segments, distribution, broken_segments):
+    distribution = [0] + distribution
+    segments = [""] + segments
+    ret = 1
+    for i in range(1, len(segments)):
+        needed = [] # build optimal from distribution
+        start = sum(distribution[:i])
+        for j in range(start, start + distribution[i]):
+            needed.append("#" * broken_segments[j])
+        optimal = ".".join(needed)
+        # print(len(segments[i]) - len(optimal), segments[i], optimal)
+        ret *= helper(len(segments[i]) - len(optimal), 0, segments[i], optimal)
     return ret
 
-part1 = solvePart1()
+
+def solve(spring, broken_segment):
+    segments = re.findall("[#?]+", spring)
+    # FOR EACH SEGMENT, SOLVE FOR NUMBER OF ORIENTATIONS
+    orientations = 0
+    # check all ways broken segments can be fitted into each segment of the spring
+    # first build a distribution of the broken segments
+    balls = len(broken_segment)
+    boxes = len(segments)
+    rng = list(range(balls + 1)) * boxes
+    distributions = list(set(i for i in itertools.permutations(rng, boxes) if sum(i) == balls))
+    print(distributions)
+    for i in range(len(distributions)):
+        orientations += naive(segments, list(distributions[i]), broken_segment)
+    return orientations
+
+
+part1 = 0
+part2 = 0
+L = len(springs)
+for i in range(L):
+    print("processing", i)
+    # solve for total number of working orientations
+    # part2 += solve("?".join([springs[i]] * 5), damaged_segments[i] * 5)
+    part1 += solve(springs[i], damaged_segments[i])
+
 print(part1)
 
-
-
+# optimal = ".".join(["#" * i for i in damaged_segments[0]])
+# print(helper(len(springs[0]) - len(optimal), springs[0], optimal, set()))
---
+++ 2023/day14.py
@@ -0,0 +1,99 @@
+from copy import copy, deepcopy
+
+file = open("../input.txt", "r")
+grid = [list(i.strip()) for i in file.readlines()]
+
+W = len(grid[0])
+H = len(grid)
+print(W, H)
+part1 = 0
+# Ideas:
+for c in range(len(grid[0])):
+    anchor = 0
+    for r in range(len(grid)):
+        if grid[r][c] == "#":
+            anchor = r + 1
+        elif grid[r][c] == "O":
+            part1 += len(grid[0]) - anchor
+            anchor += 1
+print(part1)
+
+
+# PART 2 IDEAS:
+# - simulation/graph theory to find cycle length for each round rock?
+# - east and west tilts do not affect load on north support beams (can this be used to optimize?)
+def simulate(grid):
+    temp = grid
+    all_grids = []
+    while True:
+        def tiltNorth(grid):
+            for c in range(len(grid[0])):
+                anchor = 0
+                for r in range(len(grid)):
+                    if grid[r][c] == "#":
+                        anchor = r + 1
+                    elif grid[r][c] == "O":
+                        grid[r][c] = "."
+                        grid[anchor][c] = "O"
+                        # rock_location[rock_location.index((r, c))] = (anchor, c)
+                        anchor += 1
+            return grid
+        def tiltWest(grid):
+            for r in range(len(grid)):
+                anchor = 0
+                for c in range(len(grid[0])):
+                    if grid[r][c] == "#":
+                        anchor = c + 1
+                    elif grid[r][c] == "O":
+                        grid[r][c] = "."
+                        grid[r][anchor] = "O"
+                        anchor += 1
+            return grid
+        def tiltSouth(grid):
+            for c in range(len(grid[0]) - 1, -1, -1):
+                anchor = len(grid) - 1
+                for r in range(len(grid) - 1, -1, -1):
+                    if grid[r][c] == "#":
+                        anchor = r - 1
+                    elif grid[r][c] == "O":
+                        grid[r][c] = "."
+                        grid[anchor][c] = "O"
+                        anchor -= 1
+            return grid
+        def tiltEast(grid):
+            for r in range(len(grid) - 1, -1, -1):
+                anchor = len(grid) - 1
+                for c in range(len(grid[0]) - 1, -1, -1):
+                    if grid[r][c] == "#":
+                        anchor = c - 1
+                    elif grid[r][c] == "O":
+                        grid[r][c] = "."
+                        grid[r][anchor] = "O"
+                        anchor -= 1
+            return grid
+
+        temp = tiltNorth(temp)
+        temp = tiltWest(temp)
+        temp = tiltSouth(temp)
+        temp = tiltEast(temp)
+
+        all_grids.append([row[:] for row in temp])
+        for j in range(len(all_grids) - 1):
+            if all_grids[j] == all_grids[-1]:
+                return (j, all_grids[j: -1])
+    return []
+
+part2 = 0
+stop, cycle = simulate(grid)
+# for grid in cycle:
+#     for j in grid:
+#         print("".join(j))
+#     print()
+T = 10**9 - stop
+MOD = T % len(cycle)
+for r in range(H):
+    for c in range(W):
+        if cycle[MOD - 1][r][c] == "O":
+            part2 += H - r
+
+print(part2)
