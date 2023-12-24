a29efe9c67bce94406d1042d7a9f5cb770932ce7
jonathanzhu <jiasunzhu@gmail.com>
2023-12-23 22:05:35 -0500

    Remove legacy code

--- 2023/day18.py
+++ 2023/day18.py
@@ -2,68 +2,6 @@ import math
 file = open("../input.txt", "r")
 commands = [i.strip() for i in file.readlines()]
 
-# Legacy part1 was solve for complement and then subtract from total area
-def legacyPart1():
-    W = 600
-    H = 600
-    grid = [["."] * W for _ in range(H)]
-
-    dm = {"L": (0, -1),
-          "R": (0, 1),
-          "U": (-1, 0),
-          "D": (1, 0)}
-    r = H//2
-    c = W//2
-    grid[r][c] = "#"
-    for command in commands:
-        direction, amount, colour = command.split()
-        for _ in range(int(amount)):
-            r += dm[direction][0]
-            c += dm[direction][1]
-            grid[r][c] = "#"
-
-    dr = [0, 0, -1, 1]
-    dc = [-1, 1, 0, 0]
-    def solve(start):
-        queue = []
-        queue.append(start)
-        visited = set()
-        while queue:
-            r, c = queue.pop(-1)
-            if grid[r][c] == "#":
-                continue
-
-            if (r, c) in visited:
-                continue
-            visited.add((r, c))
-            for i in range(len(dr)):
-                rr = r + dr[i]
-                cc = c + dc[i]
-                if rr < 0 or rr >= H or cc < 0 or cc >= W:
-                    continue
-                queue.append((rr, cc))
-        return len(visited)
-
-    outside = [[False] * W for _ in range(H)]
-    queue = [(0, 0)]
-
-    while queue:
-        r, c = queue.pop(-1)
-        if grid[r][c] == "#":
-            continue
-        if outside[r][c]:
-            continue
-        outside[r][c] = True
-        for i in range(len(dr)):
-            rr = r + dr[i]
-            cc = c + dc[i]
-            if rr < 0 or rr >= H or cc < 0 or cc >= W:
-                continue
-            queue.append((rr, cc))
-
-    total_area = W*H
-    return total_area - sum(sum(i) for i in outside)
-
 def find_determinant(x1, x2, y1, y2):
     return (x1*y2) - (y1*x2)
 
