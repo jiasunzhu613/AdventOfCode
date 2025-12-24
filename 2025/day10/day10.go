bd927baa099124670b47ba5deb0092ec4b78347b
jonathanzhu <jiasunzhu@gmail.com>
2025-12-24 02:34:45 -0500

    day 10

--- 2025/day10/day10.go
+++ 2025/day10/day10.go
@@ -15,13 +15,18 @@ const (
 	BASE       = 500
 )
 
+type Pattern struct {
+	cost []int
+	length int
+}
+
 // PT1
 // use binary bits to find all combinations of button presses
 // since xor is commutative, we dont need to consider repeition of same button press since
 // pressing the same button twice is equivalent to not pressing it at all
 
 // PT2
-// maybe dp, looks similar to coin change problem a little but more complicated
+// https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
 func main() {
 	file, err := os.ReadFile("../input/day10.txt")
 	if err != nil {
@@ -35,13 +40,17 @@ func main() {
 	summedLeastClicksJoltage := 0 
 	for _, line := range lines {
 		target, _ := extractTargetWithLength(line)
-		buttons, _ := extractButtons(line)
+		buttons, _, rawButtons := extractButtons(line)
 		_, joltages := extractJoltage(line)
 
-		// fmt.Println(target, buttons)
-
 		summedLeastClicks += GetLeastButtonPresses(target, 0, 0, 0, buttons)
-		summedLeastClicksJoltage += GetLeastButtonPressesJoltage(joltages, buttons)
+		
+		memo := make(map[string]int)
+		options := make([]string, 0)
+		GetButtonPressesStates(target, 0, 0, "", buttons, &options) // try all options that could make the on/off pattern
+		patterns := getAllPattern(options, len(joltages), rawButtons)
+		summedLeastClicksJoltage += GetLeastButtonPressesJoltage(joltages, buttons, rawButtons, patterns, memo)
+
 	}
 	fmt.Println("Least number of clicks need to configure all machines:", summedLeastClicks)
 	fmt.Println("Least number of clicks need to make joltage match for all machines:", summedLeastClicksJoltage)
@@ -64,27 +73,31 @@ func extractTargetWithLength(line string) (int, int) {
 }
 
 // Extracts value for button in both base 2 and base 200
-func extractButtons(line string) ([]int, []int) {
+func extractButtons(line string) ([]int, []int, [][]int) {
 	r := regexp.MustCompile(`\(([^()]*)\)`)
 	captured := r.FindAllStringSubmatch(line, -1)
 
 	values := make([]int, 0)
 	values2 := make([]int, 0)
+	raw := make([][]int, 0)
 	
 	for _, button := range captured {
 		splitted := strings.Split(button[1], ",")
 		value := 0
 		value2 := 0
+		value3 := make([]int, 0)
 		for _, b := range splitted {
 			bit, _ := strconv.Atoi(b)
 			value += int(math.Pow(2, float64(bit)))
 			value2 += int(math.Pow(BASE, float64(bit)))
+			value3 = append(value3, bit)
 		}
 		values = append(values, value)
 		values2 = append(values2, value2)
+		raw = append(raw, value3)
 	}
 
-	return values, values2
+	return values, values2, raw
 }
 
 // Express as a base X number
@@ -124,48 +137,124 @@ func GetLeastButtonPresses(target, current, consider, clicks int, buttons []int)
 	return result
 }
 
-func GetLeastButtonPressesStates(least, target, current, consider, clicks int, state string, buttons []int, best *[]string) {
-	if target == current && clicks == least{
-		*best = append(*best, state)
-		return
-	}
-
+func GetButtonPressesStates(target, current, consider int, state string, buttons []int, best *[]string) {
 	if consider == len(buttons) {
+		*best = append(*best, state)
 		return
 	}
 
 	// Consider current button
 	considered := current ^ buttons[consider]
 
-	GetLeastButtonPressesStates(least, target, considered, consider+1, clicks+1, state + "1", buttons, best)
-	GetLeastButtonPressesStates(least, target, current, consider+1, clicks, state + "0", buttons, best)
-
-	return
+	GetButtonPressesStates(target, considered, consider+1, state + "1", buttons, best)
+	GetButtonPressesStates(target, current, consider+1, state + "0", buttons, best)
 }
 
-func GetLeastButtonPressesJoltage(joltages, buttonsInBaseTwo []int) int {
-	target := 0
-	for ind, joltage := range joltages {
-		if joltage % 2 == 1 {
-			target += int(math.Pow(2, float64(ind)))
-		}
+func GetLeastButtonPressesJoltage(joltages, buttonsInBaseTwo []int, buttons [][]int, patterns []Pattern, memo map[string]int) int {
+	// Base cases + memo
+	memoClicks, ok := memo[arrayToString(joltages)]
+	if ok {
+		return memoClicks
 	}
 
-	least := GetLeastButtonPresses(target, 0, 0, 0, buttonsInBaseTwo)
+	if allZeros(joltages) {
+		return 0
+	}
 
-	best := make([]string, 0)
-	GetLeastButtonPressesStates(least, target, 0, 0, 0, "", buttonsInBaseTwo, &best)
+	if hasNegative(joltages) {
+		return BIG_NUMBER
+	}
 
-	for _, opt := range best {
-		copiedJoltage := make([]int, 0)
+	// Start: Recursive
+	leastJoltage := BIG_NUMBER
+	for _, pattern := range patterns {
+		// check parity between option and joltages
+		if !sameParity(pattern.cost, joltages) {
+			continue
+		}
+
+		copiedJoltage := make([]int, len(joltages))
 		copy(copiedJoltage, joltages)
+		// fmt.Println("copied:", copiedJoltage)
 
-		for ind, c := range opt {
-			if c == '1' {
-				for _, val := range button
-			}
+		for ind, val := range pattern.cost {
+			copiedJoltage[ind] -= val
+		}
+
+		// We dont care about any iteration where the joltages go negative
+		if hasNegative(copiedJoltage) {
+			continue
+		}	
+		
+		// Figure out how to find smalled array
+		for i := range copiedJoltage {
+			copiedJoltage[i] /= 2
+		}
+
+		clicks := 2 * GetLeastButtonPressesJoltage(copiedJoltage, buttonsInBaseTwo, buttons, patterns, memo) + pattern.length
+		leastJoltage = min(leastJoltage, clicks)
+	}
+	
+	// add to memo
+	memo[arrayToString(joltages)] = leastJoltage
+	return leastJoltage
+}
+
+func arrayToString(arr []int) string {
+	return strings.Join(strings.Fields(fmt.Sprint(arr)), ",")
+}
+
+func allZeros(arr []int) bool {
+	for _, ele := range arr {
+		if ele != 0 {
+			return false
 		}
 	}
-	fmt.Println(best)
-	return 1
+
+	return true
+}
+
+func hasNegative(arr []int) bool {
+	for _, ele := range arr {
+		if ele < 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getPattern(bits string, l int, buttons [][]int) ([]int, int) {
+	active := 0
+	change := make([]int, l)
+	for ind, c := range bits {
+		if c == '1' {
+			for _, index := range buttons[ind] {
+				change[index]++
+			}
+			active++
+		}
+	}
+
+	return change, active
+}
+
+func getAllPattern(options []string, l int, buttons [][]int) []Pattern {
+	patterns := make([]Pattern, 0)
+	for _, opt := range options {
+		cost, length := getPattern(opt, l, buttons)
+		patterns = append(patterns, Pattern{cost, length})
+	}
+
+	return patterns
+}
+
+func sameParity(a, b []int) bool{
+	for i := range a {
+		if a[i] % 2 != b[i] % 2 {
+			return false
+		}
+	}
+
+	return true
 }
